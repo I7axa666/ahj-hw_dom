2ee85702281bd7b48216c808cf16c0129135a94e
Paul Solovey <p.v.solovey@ya.ru>
2023-11-17 15:20:57 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+![CI](https://github.com/I7axa666/ahj-hw_dom/actions/workflows/web.yml/badge.svg)
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_models/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'main.html'
+    })
+  ]
+};
